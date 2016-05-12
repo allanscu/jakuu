b526ebbae80d402351f898a1a9f3ae66b500fe16
Allan Teruel <ateruel@gmail.com>
2016-05-12 12:48:20 -0700

    change default reply email to weaponiq

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,5 +1,5 @@
 class UserNotifier < ApplicationMailer
-  default :from => 'allan@juscollege.com'
+  default :from => 'support@weaponiq.com'
 
     # send a signup email to the user, pass in the user object that   contains the user's email address
     #def send_signup_email(user)
